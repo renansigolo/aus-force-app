ab82b4298e42e1b191f1e2117528dd5ba7930d58
Renan Sigolo <renan.sigolo@gmail.com>
2023-02-01 14:57:04 +1100

    add mantine

---
+++ .prettierrc.js
@@ -0,0 +1,2 @@
+/** @type {import('prettier').Config} */
+module.exports = require('eslint-config-mantine/.prettierrc.js');
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { MantineProvider } from '@mantine/core';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>AUS Force App</title>
+        <meta name="description" content="AUS Force app" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
