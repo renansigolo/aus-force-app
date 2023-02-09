ab654cd790b7e29aea85b050e009d4bb1e20946d
Renan Sigolo <renan.sigolo@gmail.com>
2023-02-09 16:01:10 +1100

    patch: env variables (#2)
    * ci: add gh action
    
    * chore: bump dependencies
    
    * build: adjust errors
    
    * ci: adjust fun name
    
    * build: code format

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAM4XUaNjZv15-kbA9Xt0fxR0p_ekEt_Zs",
-  authDomain: "aus-force.firebaseapp.com",
-  projectId: "aus-force",
-  storageBucket: "aus-force.appspot.com",
-  messagingSenderId: "295994266197",
-  appId: "1:295994266197:web:417001252718864bbcbadc",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase App
