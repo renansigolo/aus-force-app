f23e7dcb4ce8e535346425dbc29e2686379200cb
Renan Sigolo <renan.sigolo@gmail.com>
2023-02-10 12:31:50 +1100

    feat: add dashboard page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,4 +1,3 @@
-import { LockClosedIcon } from "@heroicons/react/20/solid"
 import Link from "next/link"
 
 // const SignInForm = z.object({
@@ -76,18 +75,14 @@ export default function SignIn() {
             </div>
 
             <div>
-              <button
-                type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LockClosedIcon
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                    aria-hidden="true"
-                  />
-                </span>
-                Sign in
-              </button>
+              <Link href="/dashboard">
+                <button
+                  type="button"
+                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Sign in
+                </button>
+              </Link>
             </div>
           </form>
         </div>
