aea746ed89b2fc4441a77c7068a71f411ebe3079
Renan Sigolo <renan.sigolo@gmail.com>
2023-02-17 00:14:54 +1100

    feat: add cards

---
+++ components/Card.tsx
@@ -0,0 +1,40 @@
+type CardProps = {
+  children: React.ReactNode
+}
+
+export function Card({ children }: CardProps) {
+  return (
+    <>
+      <div className="overflow-hidden rounded-lg bg-white shadow">
+        <CardHeading title="Shift #123" buttonLabel="Approve Shift" />
+        <div className="px-4 py-5 sm:p-6">{children}</div>
+      </div>
+    </>
+  )
+}
+
+type CardHeadingProps = {
+  title: string
+  buttonLabel?: string
+}
+function CardHeading({ title, buttonLabel }: CardHeadingProps) {
+  return (
+    <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
+      <div className="-ml-4 -mt-2 flex flex-wrap items-center justify-between sm:flex-nowrap">
+        <div className="ml-4 mt-2">
+          <h3 className="text-lg font-medium leading-6 text-gray-900">
+            {title}
+          </h3>
+        </div>
+
+        {buttonLabel && (
+          <div className="ml-4 mt-2 flex-shrink-0">
+            <button type="button" className="btn btn-primary capitalize">
+              {buttonLabel}
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
