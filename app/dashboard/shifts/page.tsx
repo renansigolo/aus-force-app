44aaef107d807a15b1aa41d5153d794897c153e8
Renan Sigolo <renan.sigolo@gmail.com>
2023-02-16 23:55:09 +1100

    feat: add payment

---
+++ app/dashboard/shifts/page.tsx
@@ -0,0 +1,10 @@
+import { SectionWrapper } from "@/components/dashboard/SectionWrapper"
+
+export default function ShiftsPage() {
+  return (
+    <SectionWrapper>
+      <h2 className="heading-2">Shifts</h2>
+      <section className="py-8"></section>
+    </SectionWrapper>
+  )
+}
--- components/dashboard/Navbar.tsx
+++ components/dashboard/Navbar.tsx
@@ -15,35 +15,32 @@ import {
 import { BellIcon } from "@heroicons/react/24/outline"
 import { signOut } from "firebase/auth"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 import router from "next/router"
 import { Fragment } from "react"
 import { useAuthState } from "react-firebase-hooks/auth"
 
 const navigation = [
-  { name: "Home", href: "/dashboard", icon: HomeIcon, current: true },
+  { name: "Home", href: "/dashboard", icon: HomeIcon },
   {
     name: "Weekly Jobs",
     href: "/dashboard/weekly-jobs",
     icon: CalendarDaysIcon,
-    current: false,
   },
   {
     name: "Request Leave",
     href: "/dashboard/request-leave",
     icon: BriefcaseIcon,
-    current: false,
   },
   {
-    name: "Timesheets",
-    href: "/dashboard/timesheets",
+    name: "Shifts",
+    href: "/dashboard/shifts",
     icon: ClockIcon,
-    current: false,
   },
   {
     name: "Payments",
     href: "/dashboard/payments",
     icon: BanknotesIcon,
-    current: false,
   },
 ]
 
@@ -51,6 +48,12 @@ const userNavigation = [{ name: "Your Profile", href: "/dashboard/profile" }]
 
 export function Navbar() {
   const [user] = useAuthState(auth)
+  const pathname = usePathname()
+  console.log("🚀 ~ Navbar ~ pathname", pathname)
+
+  const isActive = (href: string) => {
+    return pathname === href
+  }
 
   const signOutNow = () => {
     signOut(auth)
@@ -91,9 +94,9 @@ export function Navbar() {
                       <Link
                         key={item.name}
                         href={item.href}
-                        aria-current={item.current ? "page" : undefined}
+                        aria-current={isActive(item.href) ? "page" : undefined}
                         className={cn(
-                          item.current
+                          isActive(item.href)
                             ? "bg-gray-100 text-gray-900"
                             : "text-gray-900 hover:bg-gray-50 hover:text-gray-900",
                           "inline-flex items-center rounded-md p-3 text-sm font-medium"
@@ -173,61 +176,75 @@ export function Navbar() {
 
             {/* Mobile Dropdown Menu */}
             <Popover.Panel as="nav" className="lg:hidden" aria-label="Global">
-              <div className="mx-auto max-w-3xl space-y-1 px-2 pt-2 pb-3 sm:px-4">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? "page" : undefined}
-                    className={cn(
-                      item.current
-                        ? "bg-gray-100 text-gray-900"
-                        : "hover:bg-gray-50",
-                      "block rounded-md py-2 px-3 text-base font-medium"
-                    )}
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
+              {({ close }) => (
+                <>
+                  <div className="mx-auto max-w-3xl space-y-1 px-2 pt-2 pb-3 sm:px-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        aria-current={isActive(item.href) ? "page" : undefined}
+                        onClick={() => close()}
+                        className={cn(
+                          isActive(item.href)
+                            ? "bg-gray-100 text-gray-900"
+                            : "hover:bg-gray-50",
+                          "block rounded-md py-2 px-3 text-base font-medium"
+                        )}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
 
-              <div className="border-t border-gray-200 pt-4">
-                <div className="mx-auto flex max-w-3xl items-center px-4 sm:px-6">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src={user?.photoURL || "/images/profile-placeholder.png"}
-                      alt="Profile Image"
-                    />
-                  </div>
-                  <div className="ml-3">
-                    <div className="text-base font-medium text-gray-800">
-                      {user?.displayName || ""}
+                  <div className="border-t border-gray-200 pt-4">
+                    <div className="mx-auto flex max-w-3xl items-center px-4 sm:px-6">
+                      <div className="flex-shrink-0">
+                        <img
+                          className="h-10 w-10 rounded-full"
+                          src={
+                            user?.photoURL || "/images/profile-placeholder.png"
+                          }
+                          alt="Profile Image"
+                        />
+                      </div>
+                      <div className="ml-3">
+                        <div className="text-base font-medium text-gray-800">
+                          {user?.displayName || ""}
+                        </div>
+                        <div className="text-sm font-medium text-gray-500">
+                          {user?.email || ""}
+                        </div>
+                      </div>
+                      <button
+                        type="button"
+                        className="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      >
+                        <span className="sr-only">View notifications</span>
+                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
                     </div>
-                    <div className="text-sm font-medium text-gray-500">
-                      {user?.email || ""}
+                    <div className="mx-auto mt-3 max-w-3xl space-y-1 px-2 sm:px-4">
+                      {userNavigation.map((item) => (
+                        <Link
+                          key={item.name}
+                          href={item.href}
+                          onClick={() => close()}
+                          className="block rounded-md py-2 px-3 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900"
+                        >
+                          {item.name}
+                        </Link>
+                      ))}
+                      <button
+                        className="block w-full rounded-md py-2 px-3 text-left text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900"
+                        onClick={signOutNow}
+                      >
+                        Logout
+                      </button>
                     </div>
                   </div>
-                  <button
-                    type="button"
-                    className="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-                </div>
-                <div className="mx-auto mt-3 max-w-3xl space-y-1 px-2 sm:px-4">
-                  {userNavigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      href={item.href}
-                      className="block rounded-md py-2 px-3 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900"
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-              </div>
+                </>
+              )}
             </Popover.Panel>
           </>
         )}
