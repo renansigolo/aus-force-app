cac0fb726a57acf0b55cba71067091897d6b7e29
Renan Sigolo <renan.sigolo@gmail.com>
2023-02-17 00:15:46 +1100

    fix: shifts page

--- app/dashboard/shifts/page.tsx
+++ app/dashboard/shifts/page.tsx
@@ -4,7 +4,9 @@ export default function ShiftsPage() {
   return (
     <SectionWrapper>
       <h2 className="heading-2">Shifts</h2>
-      <section className="py-8"></section>
+      <section className="py-8">
+        <p>Coming soon...</p>
+      </section>
     </SectionWrapper>
   )
 }
