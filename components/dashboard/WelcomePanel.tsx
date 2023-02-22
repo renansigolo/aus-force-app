b196f49e43043a90d0f6ab044e8d5c2a431b6b3e
Renan Sigolo <renan.sigolo@gmail.com>
2023-02-22 23:54:48 +1100

    fix: cleanup

--- components/dashboard/WelcomePanel.tsx
+++ components/dashboard/WelcomePanel.tsx
@@ -1,11 +1,5 @@
 import { mockUser } from "@/lib/constants"
 
-const stats = [
-  { label: "Vacation days left", value: 12 },
-  { label: "Sick days left", value: 4 },
-  { label: "Personal days left", value: 2 },
-]
-
 export function WelcomePanel() {
   return (
     <section aria-labelledby="profile-overview-title">
@@ -36,17 +30,6 @@ export function WelcomePanel() {
               </div>
             </div>
           </div>
-        </div>
-        <div className="grid grid-cols-1 divide-y divide-gray-200 border-t border-gray-200 bg-gray-50 sm:grid-cols-3 sm:divide-y-0 sm:divide-x">
-          {stats?.map((stat) => (
-            <div
-              key={stat.label}
-              className="px-6 py-5 text-center text-sm font-medium"
-            >
-              <span className="text-gray-900">{stat.value}</span>{" "}
-              <span className="text-gray-600">{stat.label}</span>
-            </div>
-          ))}
         </div>
       </div>
     </section>
