6101f5bc994d1beead78d055da421a975bda46c2
Renan Sigolo <renan.sigolo@gmail.com>
2023-07-10 13:51:41 +1000

    feat: add ongoing and casual

--- app/dashboard/clients/page.tsx
+++ app/dashboard/clients/page.tsx
@@ -118,7 +118,80 @@ function Accordion({ approveShift, showModal }: any) {
 function ShiftTable({ approveShift, showModal }: any) {
   return (
     <div className="px-4 sm:px-0">
+      {/* Ongoing Table */}
       <div className="-mx-4 mt-4 sm:-mx-0">
+        <p className="mb-2 text-lg font-semibold text-gray-900">Ongoing</p>
+        <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+          <table className="min-w-full divide-y divide-gray-300">
+            <thead>
+              <tr>
+                <th
+                  scope="col"
+                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:hidden"
+                >
+                  Ongoing
+                </th>
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                >
+                  Worker Name
+                </th>
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                >
+                  Job Site
+                </th>
+
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                >
+                  End Date
+                </th>
+
+                <th scope="col" className="relative py-3.5 pl-3 pr-4">
+                  <span className="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200 bg-white">
+              {tableItems.map((tableItem) => (
+                <tr key={tableItem.name}>
+                  <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none">
+                    {tableItem.name}
+                    <dl className="font-normal lg:hidden">
+                      <dt className="sr-only">Job Site</dt>
+                      <dd className="mt-1 truncate text-gray-700">{tableItem.jobSite}</dd>
+                      <dt className="sr-only sm:hidden">End Time</dt>
+                      <dd className="mt-1 truncate text-gray-500 sm:hidden">
+                        End date: {tableItem.endDate}
+                      </dd>
+                    </dl>
+                  </td>
+                  <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
+                    {tableItem.jobSite}
+                  </td>
+                  <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
+                    {tableItem.endDate}
+                  </td>
+
+                  <td className="flex flex-col-reverse gap-2 py-4 pl-3 pr-4 text-right text-sm font-medium lg:flex-row">
+                    <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+                      14 Days Left
+                    </span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      {/* Casual Table */}
+      <div className="-mx-4 mt-4 sm:-mx-0">
+        <p className="mb-2 text-lg font-semibold text-gray-900">Casual</p>
         <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
           <table className="min-w-full divide-y divide-gray-300">
             <thead>
