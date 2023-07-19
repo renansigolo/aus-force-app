7555a006ee737af1007d5d75d258b87fb71bacf2
Renan Sigolo <renan.sigolo@gmail.com>
2023-07-19 17:12:12 +1000

    feat: add staff page

--- app/dashboard/staff/page.tsx
+++ app/dashboard/staff/page.tsx
@@ -7,13 +7,18 @@ import Modal from "@/components/Modal";
 import { Role } from "@/components/Roles";
 import NiceModal from "@ebay/nice-modal-react";
 import { Disclosure } from "@headlessui/react";
-import {
-  ChevronDownIcon,
-  ChevronRightIcon,
-  CloudArrowDownIcon,
-  DocumentTextIcon,
-  EnvelopeIcon,
-} from "@heroicons/react/20/solid";
+import { ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+
+type StaffListDataProps = {
+  email: string;
+  role: "Supervisor" | "Manager";
+};
+const staffListData: StaffListDataProps[] = [
+  { email: "vini@outlook.com", role: "Manager" },
+  { email: "renan@gmail.com", role: "Supervisor" },
+  { email: "fabi@gmail.com", role: "Supervisor" },
+];
 
 const accordionItems = [
   { title: "Job Site A" },
@@ -36,104 +41,85 @@ export default function StaffPage() {
           buttonLabel="New Staff"
           buttonAction={showModal}
         />
+
         <section className="py-8">
           <Empty title="staff" />
         </section>
 
-        <Accordion />
+        <section className="py-8">
+          <Accordion />
+        </section>
       </Role>
     </SectionWrapper>
   );
 }
 
-function Accordion({ approveShift, showModal }: any) {
+function Accordion() {
   return (
-    <div className="rounded-lg bg-white shadow">
-      <div className="mx-auto px-2 pb-6">
-        <div className="mx-auto divide-gray-900/10">
-          <dl className="space-y-6 divide-y divide-gray-900/10">
-            {accordionItems.map((item) => (
-              <Disclosure key={item.title} as="div" className="pt-6">
-                {({ open }) => (
-                  <>
-                    <dt>
-                      <Disclosure.Button className="flex w-full items-center justify-between text-left text-gray-900">
-                        <span className="flex h-7 items-center">
-                          {open ? (
-                            <ChevronDownIcon
-                              className="h-6 w-6"
-                              aria-hidden="true"
-                            />
-                          ) : (
-                            <ChevronRightIcon
-                              className="h-6 w-6"
-                              aria-hidden="true"
-                            />
-                          )}
-                          <span className="ml-2 text-base font-semibold leading-7">
-                            {item.title}
+    <>
+      {accordionItems.map((item) => (
+        <div className="rounded-lg bg-white shadow mb-2" key={item.title}>
+          <div className="mx-auto px-2 pb-6">
+            <div className="mx-auto divide-gray-900/10">
+              <dl className="space-y-6 divide-y divide-gray-900/10">
+                <Disclosure as="div" className="pt-6">
+                  {({ open }) => (
+                    <>
+                      <dt>
+                        <Disclosure.Button className="flex w-full items-center justify-between text-left text-gray-900">
+                          <span className="flex h-7 items-center">
+                            {open ? (
+                              <ChevronDownIcon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            ) : (
+                              <ChevronRightIcon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            )}
+                            <span className="ml-2 text-base font-semibold leading-7">
+                              {item.title}
+                            </span>
                           </span>
-                        </span>
-                      </Disclosure.Button>
-                    </dt>
-                    <Disclosure.Panel as="dd" className="mt-2 px-2">
-                      <StaffList />
-                      {/* <ShiftTable approveShift={approveShift} showModal={showModal} /> */}
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
-            ))}
-          </dl>
+                        </Disclosure.Button>
+                      </dt>
+                      <Disclosure.Panel as="dd" className="mt-2 px-2">
+                        {staffListData.map((item) => (
+                          <StaffList {...item} key={item.email} />
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+              </dl>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
 
-function StaffList() {
+function StaffList(props: StaffListDataProps) {
+  const { email, role } = props;
+
   return (
     <div className="mb-2 rounded-md border border-gray-200 bg-white px-4 py-5 sm:px-6">
       <div className="-ml-4 -mt-4 flex flex-wrap items-center justify-between sm:flex-nowrap">
         <div className="ml-4 mt-4">
           <div className="flex items-center">
             <div className="flex-shrink-0">
-              <DocumentTextIcon className="h-12 w-12" />
+              <UserCircleIcon className="h-12 w-12" />
             </div>
             <div className="ml-4">
               <h3 className="text-base font-semibold leading-6 text-gray-900">
-                INV-123
+                {role}
               </h3>
-              <p className="text-sm text-gray-500">
-                <a href="#">
-                  {/* Due on 29/10/2023 */}
-                  Paid 29/10/2023
-                </a>
-              </p>
+              <p className="text-sm text-gray-500">{email}</p>
             </div>
           </div>
-        </div>
-        <div className="ml-4 mt-4 flex flex-shrink-0">
-          <button
-            type="button"
-            className="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          >
-            <CloudArrowDownIcon
-              className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-              aria-hidden="true"
-            />
-            <span>Download</span>
-          </button>
-          <button
-            type="button"
-            className="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          >
-            <EnvelopeIcon
-              className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-              aria-hidden="true"
-            />
-            <span>Email</span>
-          </button>
         </div>
       </div>
     </div>
