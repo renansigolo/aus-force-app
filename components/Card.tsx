2628a9602cfecc75db559789ccd5e752e4a5f91a
Renan Sigolo <renan.sigolo@gmail.com>
2023-10-01 00:48:41 +1000

    refactor: card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,47 +1,41 @@
 import { HTMLAttributes, forwardRef } from "react"
 import { twMerge } from "tailwind-merge"
 
-const Card = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={twMerge(
-        "divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow",
-        className,
-      )}
-      {...props}
-    />
-  ),
-)
+type CardProps = HTMLAttributes<HTMLDivElement>
+
+const Card = forwardRef<HTMLDivElement, CardProps>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={twMerge(
+      "divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow",
+      className,
+    )}
+    {...props}
+  />
+))
 Card.displayName = "Card"
 
-const CardHeader = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={twMerge("flex items-center justify-between gap-1 px-4 py-5 sm:px-6", className)}
-      {...props}
-    />
-  ),
-)
+const CardHeader = forwardRef<HTMLDivElement, CardProps>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={twMerge("flex items-center justify-between gap-1 px-4 py-5 sm:px-6", className)}
+    {...props}
+  />
+))
 CardHeader.displayName = "CardHeader"
 
-const CardContent = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div ref={ref} className={twMerge("flex gap-2 px-4 py-5 sm:p-6", className)} {...props} />
-  ),
-)
+const CardContent = forwardRef<HTMLDivElement, CardProps>(({ className, ...props }, ref) => (
+  <div ref={ref} className={twMerge("flex gap-2 px-4 py-5 sm:p-6", className)} {...props} />
+))
 CardContent.displayName = "CardContent"
 
-const CardFooter = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={twMerge("flex flex-row-reverse px-4 py-4 sm:px-6", className)}
-      {...props}
-    />
-  ),
-)
+const CardFooter = forwardRef<HTMLDivElement, CardProps>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={twMerge("flex flex-row-reverse px-4 py-4 sm:px-6", className)}
+    {...props}
+  />
+))
 CardFooter.displayName = "CardFooter"
 
 export { Card, CardContent, CardFooter, CardHeader }
