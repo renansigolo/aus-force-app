4a18d43a2683733d6f846b595edc44bfca952771
Renan Sigolo <renan.sigolo@gmail.com>
2023-11-30 22:59:21 +1100

    fix: config

--- release.config.js
+++ release.config.js
@@ -4,6 +4,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    ["@semantic-release/npm", { npmPublish: false }],
     "@semantic-release/github",
     "@semantic-release/git",
   ],
