dbc2789ceb36000f94182120b0c07010eb9a5640
Renan Sigolo <renan.sigolo@gmail.com>
2023-11-30 22:10:55 +1100

    ci: add semantic config

--- release.config.js
+++ release.config.js
@@ -1,4 +1,10 @@
 /** @type {import('semantic-release').Options} */
 module.exports = {
   branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    "@semantic-release/git",
+  ],
 }
